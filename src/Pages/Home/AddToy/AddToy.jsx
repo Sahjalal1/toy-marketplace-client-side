72aec34726baa4c4bcc2a93d825596fa0c7f3453
rahim <mdsahjalalrahim@gmail.com>
2023-05-19 16:49:21 +0600

    all toys route added

--- src/Pages/Home/AddToy/AddToy.jsx
+++ src/Pages/Home/AddToy/AddToy.jsx
@@ -1,17 +1,114 @@
 
 
 const AddToy = () => {
+
+    const handleSubmit = event =>{
+        event.preventDefault()
+
+        const form = event.target;
+        const photo = form.photo.value;
+        const toyname = form.toyname.value;
+        const category =form.category.value;
+        const price =form.price.value;
+        const rating =form.rating.value;
+        const sellername =form.sellername.value;
+        const selleremail =form.selleremail.value;
+        const quantity =form.quantity.value;
+        const description =form.description.value;
+        console.log(photo, toyname, category, price, rating, sellername, selleremail, quantity, description)
+    }
+
     return (
-        <div>
-
-            <form className=" py-10 px-16 mx-auto" style={{ backgroundImage: `url("https://www.shutterstock.com/image-photo/old-brick-black-color-wall-260nw-1605128917.jpg")` }}>
-                <div className="flex mx-auto w-[90%]">
-                    <h1>Email</h1>
-                    <input type="text" placeholder="Type here" className="py-3  rounded-sm input-bordered input-error w-full max-w-xs" />
+        <div className="h-auto" style={{ backgroundImage: `url("https://www.shutterstock.com/image-photo/old-brick-black-color-wall-260nw-1605128917.jpg")` }}>
 
 
-                    <h1>Email</h1>
-                    <input type="text" placeholder="Type here" className="py-3 px-5 rounded-sm input-bordered input-error w-full max-w-xs" />
+            <form onSubmit={handleSubmit} className="w-[80%]  lg:py-10 lg:px-16 mx-auto" >
+                <h1 className="text-3xl text-center font-extrabold">AddCoffee</h1>
+            {/* img URL div */}
+                <div className="md:flex mb-7">
+                    <div className="form-control md:w-full ">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">ImageURL</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="photo" placeholder="ImageURL" className="input input-bordered input-error w-full " />
+                        </label>
+                    </div>
+                </div>
+            {/* Toy Name and  Sub-Category*/}
+                <div className="md:flex mb-7">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Toy Name</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="toyname" placeholder="Toy Name" className="input input-bordered input-error w-full " />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 lg:ml-4">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Sub-Category</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="category" placeholder="Sub-Category " className="input input-bordered input-error w-full " />
+                        </label>
+                    </div>
+                </div>
+        {/* Price and  Rating*/}
+                <div className="md:flex mb-7">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Price</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="price" placeholder="Price" className="input input-bordered input-error w-full " />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 lg:ml-4">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Rating</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="rating" placeholder="Rating " className="input input-bordered input-error w-full " />
+                        </label>
+                    </div>
+                </div>
+            {/* Seller Name and Seller Email */}
+                <div className="md:flex mb-7">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Seller Name</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="sellername" placeholder="Seller Name" className="input input-bordered input-error w-full " />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 lg:ml-4">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Seller Email</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="selleremail" placeholder="Seller Email " className="input input-bordered input-error w-full " />
+                        </label>
+                    </div>
+                </div>
+            {/* Detail description and Available quantity and submit btn*/}
+                <div className="md:flex mb-7">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Detail description</h1>
+                        </label>
+                        <textarea className="textarea textarea-error lg:pb-[100px]"  name="description" placeholder="Detail description"></textarea>
+                    </div>
+                    <div className="form-control md:w-1/2 lg:ml-4">
+                        <label className="label">
+                            <h1 className="text-xl text-white font-extrabold">Available quantity</h1>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="quantity" placeholder="Available quantity " className="input input-bordered input-error w-full " />
+                        </label>
+                        <input className="mt-12 py-5 text-xl bg-white btn-outline btn-error" type="submit" value="Add TOY" />
+                    </div>
                 </div>
 
             </form>
--- src/Pages/Home/AllToys/AllToys.jsx
+++ src/Pages/Home/AllToys/AllToys.jsx
@@ -1,31 +1,107 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+import { useState } from "react";
 
 
 const AllToys = () => {
-    const [alltoys, setAllToys] = useState([])
-    const [toys, setToys] = useState([])
+    // const [alltoys, setAllToys] = useState([])
+    // const [toys, setToys] = useState([])
 
-    useEffect(() => {
-        fetch('ppp.json')
-            .then(res => res.json())
-            .then(data => {
-                const datas = data.slice(0, 20)
-                setToys(datas)
-                setAllToys(data)
-            })
-    }, [])
+    // useEffect(() => {
+    //     fetch('ppp.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const datas = data.slice(0, 20)
+    //             setToys(datas)
+    //             setAllToys(data)
+    //         })
+    // }, [])
 
-    const haha = () => {
-        setToys(alltoys)
-    }
+    // const haha = () => {
+    //     setToys(alltoys)
+    // }
+    // const [model, setModel] = useState([])
+
+    // const haha = id =>{
+
+    // }
     return (
-        <div>
-            <h1>{toys.length}</h1>
-            {
+        <>
+            <div>
+                {/* <h1>{toys.length}</h1> */}
+                {/* {
                 toys.map(rr => <p key={rr.id}>{rr.id}</p>)
-            }
-            <button onClick={haha}>fkdjf</button>
-        </div>
+            } */}
+                {/* <button onClick={haha}>fkdjf</button> */}
+            </div>
+            <div>
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+
+                        <thead>
+                            <tr>
+                                <th>Toys Image</th>
+                                <th>Toys Name and Price</th>
+                                <th>Seller Name and Sub-category</th>
+                                <th>Available Quantity</th>
+                                <th>Favorite Color</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div className="flex items-center ">
+                                        <div className="avatar">
+                                            <div className="mask ">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvnmld4IhNGHG1pz3geu_W3mX0BOTczG4EAw&usqp=CAU" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <div className="font-bold">Toys Name</div>
+                                        <div className="text-sm opacity-50">Price</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    sal name
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">catagory</span>
+                                </td>
+                                <td>Quantity</td>
+                                <th>
+
+                                    <button className="btn btn-outline btn-error">Error</button>
+                                    {/* <label onClick={haha} htmlFor="my-modal-5" className="btn">open modal</label> */}
+                                </th>
+                            </tr>
+                        </tbody>
+
+
+                    </table>
+                </div>
+            </div>
+            {/* <button onClick={haha}>fkdjf</button> */}
+            <div className="w-40 mx-auto">
+                <button className="btn">rahik</button>
+            </div>
+
+
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box w-11/12 max-w-5xl">
+                    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+                    <p className="py-4"> been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <div className="modal-action">
+                        <label htmlFor="my-modal-5" className="btn">Yay!</label>
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 };
 
