b8952bc515619203ac91db3694d5e008b128647f
rahim <mdsahjalalrahim@gmail.com>
2023-05-20 00:09:47 +0600

    MYToys running

--- src/Pages/Home/AllToys/AllToys.jsx
+++ src/Pages/Home/AllToys/AllToys.jsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from "react";
-import Toysmap from "./Toysmap";
+import {  useEffect, useState } from "react";
+// import { AuthContext } from "../../../Providers/AuthProviders";
+
 
 
 
 
 
 const AllToys = () => {
-
+// const {loading} = useContext(AuthContext)
 
     const [alltoys, setAllToys] = useState([])
     const [toys, setToys] = useState([])
 
     useEffect(() => {
+        
         fetch('http://localhost:5000/toys')
             .then(res => res.json())
             .then(data => {
@@ -25,16 +27,60 @@ const AllToys = () => {
         setToys(alltoys)
     }
 
-console.log(toys)
+
+    const model = id =>{
+        const fl = alltoys.filter(alltoys._id === id)
+        console.log(fl)
+    }
     return (
         <>
-        <div>
-            {
-                toys.map(toy => <Toysmap key={toy._id} toy={toy}></Toysmap>)
-            }
-        </div>
-        
-            
+            <div>
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+
+                        <thead>
+                            <tr>
+                                <th>Toys Name</th>
+                                <th>Seller Name</th>
+                                <th>category</th>
+                                <th>Price</th>
+                                <th>quantity</th>
+                                <th>button</th>
+                            </tr>
+                        </thead>
+
+                        {
+                            toys.map(toy => <tbody key={toy._id}>
+
+                                <tr>
+                                    <td>
+                                        <div className="font-bold">{toy.toyname}</div>
+                                    </td>
+                                    <td>
+                                        <span className="badge badge-ghost badge-sm">{toy.sellername}</span>
+                                    </td>
+                                    <td>
+                                        {toy.category}
+                                    </td>
+                                    <th>
+                                        {toy.price}
+                                    </th>
+                                    <th>
+                                        {toy.quantity}
+                                    </th>
+                                    <th>
+                                        <label onClick={()=> model (toy._id)} htmlFor="my-modal-5" className="btn btn-outline btn-error">Toys Details</label>
+                                    </th>
+                                </tr>
+
+                            </tbody>)
+                        }
+
+                    </table>
+                </div>
+            </div>
+
+
             {/* <button onClick={haha}>fkdjf</button> */}
             <div className="w-40 mx-auto">
                 <button onClick={haha} className="btn">All Toys</button>
--- src/Pages/Home/AllToys/Toysmap.jsx
+++ src/Pages/Home/AllToys/Toysmap.jsx
@@ -1,48 +1,12 @@
 
 
-const Toysmap = ({toy}) => {
-    const {toyname, price, sellername, category, quantity, } = toy
+const Toysmap = ({ toy }) => {
+    const { toyname, price, sellername, category, quantity, } = toy
     return (
-        <><div className="overflow-x-auto w-full">
-            <table className="table w-full">
-
-                <thead>
-                    <tr>
-                        <th>Toys Name</th>
-                        <th>Seller Name</th>
-                        <th>category</th>
-                        <th>Price</th>
-                        <th>quantity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>                          
-                            <div className="font-bold">{toyname}</div>
-                        </td>
-                        <td>
-                            <span className="badge badge-ghost badge-sm">{sellername}</span>
-                        </td>
-                        <td>
-                            {category}
-                        </td>
-                        <th>
-                            {price}
-                        </th>
-                        <th>
-                            {quantity}
-                        </th>
-                        <th>
-
-                            <button className="btn btn-outline btn-error">Error</button>
-                            {/* <label onClick={haha} htmlFor="my-modal-5" className="btn">open modal</label> */}
-                        </th>
-                    </tr>
-                </tbody>
-
-
-            </table>
-        </div>
+        <>
+            
+                
+            
         </>
     );
 };
